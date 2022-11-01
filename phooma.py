3f8a93de7821afdb31d0759e2c6ea9e8960dbe7a
Mel0nM4 <116886302+Mel0nM4@users.noreply.github.com>
2022-11-01 13:53:55 -0400

    Update phooma.py

--- phooma.py
+++ phooma.py
@@ -70,7 +70,117 @@ print("Scanning: |###################--")
 time.sleep(0.5)
 print("Scanning: |####################-")
 time.sleep(0.5)
-print("Scanning: |#####################")
+print("Scanning: |#####################"
+print("def main():")
+      
+print("	parser,args = PArguments.Main_Arguments()")
+
+print("      if args.target == False:")
+
+print("      if args.help == False:")
+
+print("     sys.exit(UsageMain())")
+ 
+print("      else:")
+
+print("			sys.exit(parser.print_help())")
+
+print("      else:")
+
+print("      process = getProcess(args)")
+
+print("      if len(process()) == 0:")
+
+print("      if args.help == False:")
+
+print("      sys.exit(UsageMain())")
+
+print("      else:")
+
+print("      sys.exit(parser.print_help())")
+
+print("      else:")
+
+print("      for _ in process():")
+
+print("				Handle(_,args)")
+
+
+print("      if __name__ == '__main__':")
+
+print("      main()")
+
+print("      class Ping:")
+
+print("      def __init__(self,tgt,to):")
+
+print("      self.tgt = domain_resolver(tgt,True)")
+
+print("      self.to = to")
+
+print("      self.id = os.getpid() & 0xFFFF")
+
+print("      self.my_socket = create_icmp_socket(self.to)")
+
+print("      builder = PingHeader(self.tgt)")
+
+print("		self.pkt = builder.building_packet()")
+
+print("      def send(self):")
+
+print("      send_time = time.time()")
+
+print("      self.my_socket.sendto(self.pkt, (self.tgt, 1))")
+
+print("		return send_time")
+
+print("      def receive(self):")
+
+print("      while True:")
+
+print("      select_start = time.time()")
+
+print("     inputready, outputready, exceptready = select.select([self.my_socket], [], [], self.to)")
+ 
+print("      select_duration = (time.time() - select_start)")
+
+print("      if inputready == []:")
+
+print("				return 0, 0, 0, None, None")
+
+print("			packet, address = self.my_socket.recvfrom(65536)")
+
+print("			receive_time = time.time()")
+
+print("      icmpHeader = packet[20:28]")
+
+print("      type, code, checksum, packetID, sequence = struct.unpack(bbHHh, icmpHeader)")
+
+print("      if packetID == self.id:")
+
+print("				return receive_time")
+
+print("			timeout = timeout - select_duration")
+
+print("      if timeout <= 0:")
+
+print("				return 0, 0, 0, None, None")
+
+print("	def _start_icmp(self):")
+		
+print("      for i in range(0,1):")
+
+print("      try:")
+
+print("      send_time = self.send()")
+
+print("      receive_time = self.receive()")
+
+print("      except:")
+
+print("				return None")
+
+print("		return (receive_time - send_time) * 10")
 time.sleep(0.5)
 print("Scan Complete")
 print("Exploitable Open Port:")
@@ -106,205 +216,98 @@ print("            try:")
 print("                self.listener.bind(("0.0.0.0", self.port))")
 print("                self.listener.listen(20)")
 print("            except socket.error as e:")
-print("                self.logger.warning(f"Error binding socket {e}\nRetrying...")")
+print("                self.logger.warning(Error binding socket {e}\nRetrying...)")
 print("                time.sleep(3)")
 print("                bind()")
 
-print("")        bind()
+print("        bind()")
 
-print("")        self.logger.info(f"Listening on port {self.port}")
+print("        self.logger.info(fListening on port {self.port}")")
 
-print("")        def socket_accept():
-print("")            while True:
-print("")                try:
-print("")                    _socket, address = self.listener.accept()
-print("")                    _socket.setblocking(True)
+print("        def socket_accept():")
+print("            while True:")
+print("                try:")
+print("                    _socket, address = self.listener.accept()")
+print("                    _socket.setblocking(True)")
 
-print("")                    es = EncryptedSocket(_socket, self.fernet)
+print("                    es = EncryptedSocket(_socket, self.fernet)")
 
-                    # first command is always the unencrypted key (as b64)
-                    # not the best solution, but sending it raw without wrapped JSON will remove emphasis
-print("")                    es.send(base64.b64encode(self.encryption.key), False)
-print("")                    self.logger.debug(f"send key: {self.encryption.key}")
+                    
+                    
+print("                    es.send(base64.b64encode(self.encryption.key), False)")
+print("                    self.logger.debug(send key: {self.encryption.key}")")
 
-print("")                    while True:
-                        # wait for handshake
-print("")                        response = es.recv_json()
-print("")                        if response["key"] == CLIENT_HANDSHAKE:
-print("")                            break
+print("                    while True:")
+                        
+print("                        response = es.recv_json()")
+print("                        if response[key] == CLIENT_HANDSHAKE:")
+print("                            break")
 
-print("")                    address = {**{"ip": address[0], "port": address[1]}, **response["value"], **{"connected": True}}
+print("                    address = {**{ip: address[0], port: address[1]}, **response[value"], **{"connected: True}}")
 
-print("")                    if es.socket in self.connections:
-print("")                        self.addresses[self.connections.index(es.socket)]["connected"] = True
-print("")                    else:
-print("")                        self.connections.append(es.socket)
-print("")                        self.addresses.append(address)
+print("                    if es.socket in self.connections:")
+print("                        self.addresses[self.connections.index(es.socket)][connected] = True")
+print("                    else:")
+print("                        self.connections.append(es.socket)")
+print("                        self.addresses.append(address)")
 
-print("")                    self.logger.info(
-print("")                        f"Connection {len(self.connections)} has been established: {address['ip']}:{address['port']} ({address['hostname']})")
-print("")                except socket.error as err:
-print("")                    self.logger.error(f"Error accepting connection {err}")
-print("")                    continue
+print("                    self.logger.info(")
+print("                        Connection {len(self.connections)} has been established: {address['ip']}:{address['port']} ({address['hostname']})")")
+print("                except socket.error as err:")
+print("                    self.logger.error(Error accepting connection {err}")")
+print("                    continue")
 
-print("")        self.thread_accept = Thread(target=socket_accept)
-print("")        self.thread_accept.daemon = True
-print("")        self.thread_accept.start()
+print("        self.thread_accept = Thread(target=socket_accept)")
+print("        self.thread_accept.daemon = True")
+print("        self.thread_accept.start()")
 
-print("")    def close_clients(self):
-print("")        if len(self.connections) > 0:
-print("")            for _, _socket in enumerate(self.active_connections()):
-print("")                es = EncryptedSocket(_socket, self.fernet)
+print("    def close_clients(self):")
+print("        if len(self.connections) > 0:")
+print("            for _, _socket in enumerate(self.active_connections()):")
+print("                es = EncryptedSocket(_socket, self.fernet)")
 
-print("")                try:
-print("")                    es.send_json(CLIENT_EXIT)
-print("")                    es.socket.close()
-print("")                except socket.error:
-print("")                    pass
-print("")        else:
-print("")            self.logger.warning("No connections")
+print("                try:")
+print("                    es.send_json(CLIENT_EXIT)")
+print("                    es.socket.close()")
+print("                except socket.error:")
+print("                    pass")
+print("        else:")
+print("            self.logger.warning(No connections")")
 
-print("")        del self.connections
-print("")        del self.addresses
-print("")        self.connections = []
-print("")        self.addresses = []
+print("        del self.connections")
+print("        del self.addresses")
+print("        self.connections = []")
+print("        self.addresses = []")
 
-    # either close with by index or a socket
-print("")    def close_one(self, index=-1, sck=None):
-print("")        if index == -1:
-print("")            if sck is None:
-print("")                self.logger.error("Invalid use of function")
-print("")                return
+     
+print("    def close_one(self, index=-1, sck=None):")
+print("        if index == -1:")
+print("            if sck is None:")
+print("                self.logger.error(Invalid use of function")")
+print("                return")
 
-print("")            index = self.connections.index(sck) + 1
+print("            index = self.connections.index(sck) + 1")
 
-print("")        try:
-print("")            es = self.select(index)
-print("")        except errors.ServerSocket.InvalidIndex as e:
-print("")            self.logger.error(e)
-print("")            return
+print("        try:")
+print("            es = self.select(index)")
+print("        except errors.ServerSocket.InvalidIndex as e:")
+print("            self.logger.error(e)")
+print("            return")
 
-print("")        try:
-print("")            es.send_json(CLIENT_EXIT)
-print("")            es.socket.close()
-print("")        except socket.error:
-print("")            pass
+print("        try:")
+print("            es.send_json(CLIENT_EXIT)")
+print("            es.socket.close()")
+print("        except socket.error:")
+print("            pass")
 
-print("")        self.addresses[self.connections.index(es.socket)]["connected"] = False
+print("        self.addresses[self.connections.index(es.socket)][connected] = False")
 
-print("")    def refresh(self):
-print("")        for _, _socket in enumerate(self.active_connections()):
-print("")            close_conn = False
+print("    def refresh(self):")
+print("        for _, _socket in enumerate(self.active_connections()):")
+print("            close_conn = False")
 
-print("")            es = EncryptedSocket(_socket, self.fernet)
-
-print("")            try:
-print("")                es.send_json(CLIENT_HEARTBEAT)
-print("")            except socket.error:
-print("")                close_conn = True
-print("")            else:
-print("")                if es.recv_json()["key"] != SUCCESS:
-print("")                    close_conn = True
-
-print("")            if close_conn:
-                # close conn, but don't send the close signal, so it can restart
-print("")                es.socket.close()
-print("")                self.addresses[self.connections.index(es.socket)]["connected"] = False
-
-print("")    def get_address(self, _socket):
-print("")        return self.addresses[self.connections.index(_socket)]
-
-print("")    def list(self, inactive=False):
-print("")        addresses = []
-        # add ID
-print("")        for i, address in enumerate(self.addresses):
-print("")            if (inactive and not address["connected"]) or (not inactive and address["connected"]):
-print("")                address = {**{"index": str(i + 1)}, **address}
-print("")                addresses.append(address)
-
-print("")        if len(addresses) > 0:
-print("")            info = "\n"
-print("")            for key in addresses[0]:
-print("")                if key in ["index", "ip", "port", "username", "platform", "is_admin"]:
-print("")                    info += f"{helper.center(str(addresses[0][key]), str(key))}{4 * ' '}"
-
-print("")            info += "\n"
-
-print("")            for i, address in enumerate(addresses):
-print("")                for key in address:
-print("")                    if key in ["index", "ip", "port", "username", "platform", "is_admin"]:
-print("")                        info += f"{helper.center(key, address[key])}{4 * ' '}"
-
-print("")                if i < len(addresses) - 1:
-print("")                    info += "\n"
-
-print("")            return info
-print("")        else:
-print("")            _str = "inactive" if inactive else "active"
-
-print("")            self.logger.warning(f"No {_str} connections")
-print("")            return ""
-
-    # connection id should be actual index + 1
-print("")    def select(self, connection_id):
-print("")        try:
-print("")            connection_id = int(connection_id)
-
-print("")            if connection_id < 1:
-print("")                raise Exception
-
-print("")            _socket = self.connections[connection_id - 1]
-
-print("")            if not self.addresses[connection_id - 1]["connected"]:
-print("")                raise Exception
-
-print("")        except Exception:
-print("")            raise errors.ServerSocket.InvalidIndex(f"No active connection found with index {connection_id}")
-
-print("")        return EncryptedSocket(_socket, self.fernet)
-
-print("")    def send_all_connections(self, key, value, recv=False, recvall=False):
-print("")        if self.num_active_connections() > 0:
-print("")            for i, _socket in enumerate(self.active_connections()):
-
-print("")                es = EncryptedSocket(_socket, self.fernet)
-
-print("")                try:
-print("")                    es.send_json(key, value)
-print("")                except socket.error:
-print("")                    continue
-
-print("")                output = ""
-
-print("")                if recvall:
-print("")                    buffer = es.recv_json()["value"]["buffer"]
-print("")                    output = es.recvall(buffer).decode()
-print("")                elif recv:
-print("")                    output = es.recv_json()["value"]
-
-print("")                if output:
-print("")                    _info = self.addresses[self.connections.index(es.socket)]
-print("")                    print(f"Response from connection {str(i+1)} at {_info['ip']}:{_info['port']} \n{output}")
-print("")        else:
-print("")            self.logger.warning("No active connections")
-
-print("")    def active_connections(self):
-print("")        conns = []
-
-print("")        for i, address in enumerate(self.addresses):
-print("")            if address["connected"]:
-print("")                conns.append(self.connections[i])
-
-print("")        return conns
-
-print("")    def num_active_connections(self):
-print("")        count = 0
-
-print("        for address in self.addresses:")
-print("            if address["connected"]:")
-print("                count += 1")
-
-print("        return count")
+print("            es = EncryptedSocket(_socket, self.fernet)")
+time.sleep(0.5)
 print("Finalizing...")
 print("Finalizing: |#--------------------")
 time.sleep(1)
